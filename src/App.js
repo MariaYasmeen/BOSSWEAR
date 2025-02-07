11ab106c664fa61dd837ca6fd3f0bd906c454b1a
Maria Yasmeen <mariyayasmeen000@gmail.com>
2025-02-07 20:57:35 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+import './App.css';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="gradient-bg  align-items-center vh-100">
+     <Home />
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbr.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const navLinks = [
+  { name: "Home", path: "#", active: true },
+  { name: "Link", path: "#" },
+  { name: "Dropdown", dropdown: ["Action", "Another action", "Something else here"] },
+  { name: "Disabled", path: "#", disabled: true },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg py-4 px-4 bg-transparent">
+      <div className="container-fluid">
+        <a className="navbar-brand mx-3" href="#">Navbar</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarScroll">
+          <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+            {navLinks.map((link, index) => (
+              link.dropdown ? (
+                <li key={index} className="nav-item dropdown mx-3">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                    {link.name}
+                  </a>
+                  <ul className="dropdown-menu">
+                    {link.dropdown.map((item, i) => (
+                      <li key={i}>
+                        <a className="dropdown-item" href="#">{item}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ) : (
+                <li key={index} className="nav-item mx-3">
+                  <a className={`nav-link ${link.active ? "active" : ""} ${link.disabled ? "disabled" : ""}`} href={link.path}>
+                    {link.name}
+                  </a>
+                </li>
+              )
+            ))}
+          </ul>
+          <form className="d-flex mx-3">
+            <input className="form-control me-2" type="search" placeholder="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
